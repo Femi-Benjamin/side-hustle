eae6fb048380609e29bf788aea5dca5fa879e249
iamolufemijp <benjamini16@gmail.com>
2022-05-23 22:47:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// var num1 = 6;
+// var name = 'john';
+
+// console.log(num1);
+// console.log(name);
+// console.log('john doe')
+
+// var x = 50;
+// var y = 10;
+// var sum = x * y;
+// console.log(sum);
+
+// //  case sensitivity
+// var num2 = 6;
+// var Num = 10;
+// var difference = Num - num2;
+// console.log(difference);
+
+// for (let i = 0; i < 5; i++) {
+//           console.log(i)
+
+// }
+
+var div=document.getElementById('div1first');
+div.style.backgroundColor='brown';
